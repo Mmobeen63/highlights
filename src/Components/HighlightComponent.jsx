73508e2116147e91d44dd28c582da367a95be86a
Mohammad Mobeen <mobeeen2@outlook.com>
2023-01-30 18:26:17 +0500

    ..

--- src/Components/HighlightComponent.jsx
+++ src/Components/HighlightComponent.jsx
@@ -2,31 +2,22 @@ import React, { useState, useRef, useEffect } from "react";
 
 function AnnotationTool({ children }) {
   const [selectedText, setSelectedText] = useState("");
+  const [selectedColor, setSelectedColor] = useState("yellow");
+  const [showModal, setShowModal] = useState(false);
   const textRef = useRef(null);
 
-  useEffect(() => {
-    const handleSelection = (e) => {
-      const selected = window.getSelection().toString();
-      if (selected.length > 0) {
-        setSelectedText(selected);
-      }
-    };
-
-    textRef.current.addEventListener("mouseup", handleSelection);
-
-    return () => {
-      textRef.current.removeEventListener("mouseup", handleSelection);
-    };
-  }, []);
+  // ...
 
   const handleHighlight = () => {
     const selection = window.getSelection();
     if (selection.rangeCount > 0) {
       const range = selection.getRangeAt(0);
       const highlight = document.createElement("span");
+      highlight.style.backgroundColor = selectedColor;
       highlight.classList.add("highlight");
       highlight.appendChild(range.extractContents());
       range.insertNode(highlight);
+      localStorage.setItem("highlightedText", highlight.textContent);
     }
   };
 
@@ -34,7 +25,41 @@ function AnnotationTool({ children }) {
     <div>
       <span ref={textRef} dangerouslySetInnerHTML={{ __html: children }}></span>
       <div>
+        <button onClick={() => setShowModal(true)}>Select Color</button>
         <button onClick={handleHighlight}>Highlight</button>
+        {showModal && (
+          <div className="modal">
+            <div className="modal-content">
+              <div>
+                <h3>Select Color</h3>
+                <div className="color-picker">
+                  <div
+                    className="color-option"
+                    onClick={() => setSelectedColor("yellow")}
+                    style={{ backgroundColor: "yellow" }}
+                  >
+                    yellow
+                  </div>
+                  <div
+                    className="color-option"
+                    onClick={() => setSelectedColor("pink")}
+                    style={{ backgroundColor: "pink" }}
+                  >
+                    pink
+                  </div>
+                  <div
+                    className="color-option"
+                    onClick={() => setSelectedColor("lightgreen")}
+                    style={{ backgroundColor: "lightgreen" }}
+                  >
+                    green
+                  </div>
+                </div>
+                <button onClick={() => setShowModal(false)}>Close</button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
